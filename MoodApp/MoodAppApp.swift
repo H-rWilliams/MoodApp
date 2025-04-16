1c49cdd7b2508fa3565b594eddc5bc214a7fde6a
Hunter Williams <hw0923309@otc.edu>
2025-04-16 08:40:26 -0500

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by WILLIAMS, HUNTER R. on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodAppTests/MoodAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppTests.swift
+//  MoodAppTests
+//
+//  Created by WILLIAMS, HUNTER R. on 4/16/25.
+//
+
+import Testing
+@testable import MoodApp
+
+struct MoodAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
